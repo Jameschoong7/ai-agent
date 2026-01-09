e0b73f64990d1f70b1438fbb0f1df842b91eecb7
Jameschoong7 <jameschoong7@gmail.com>
2026-01-10 01:51:33 +0800

    add  calculator function and test get file function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from google import genai
 import os 
 from dotenv import load_dotenv
+from google.genai import types
 import argparse
 
 
@@ -12,12 +13,16 @@ client = genai.Client(api_key=api_key)
 def main():
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt",type=str,help="User prompt")
+    parser.add_argument("--verbose",action="store_true",help="Enable verbose output")
     args = parser.parse_args()
-    generate_content = client.models.generate_content(model="gemini-2.5-flash",contents=args.user_prompt)
+    messages = [types.Content(role="user",parts=[types.Part(text=args.user_prompt)])]
+    generate_content = client.models.generate_content(model="gemini-2.5-flash",contents=messages)
     if not generate_content.usage_metadata:
         raise RuntimeError("Gemini API response appears t obe malformed")
-    print("Prompt tokens: ",generate_content.usage_metadata.prompt_token_count)
-    print("Response tokens: ",generate_content.usage_metadata.candidates_token_count)
+    if args.verbose:
+        print("User prompt: ",args.user_prompt)
+        print("Prompt tokens: ",generate_content.usage_metadata.prompt_token_count)
+        print("Response tokens: ",generate_content.usage_metadata.candidates_token_count)
     print("Response:\n",generate_content.text)
 
 if __name__ == "__main__":
